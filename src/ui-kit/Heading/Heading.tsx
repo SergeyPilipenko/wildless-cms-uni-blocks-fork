d1dd11fa64925b6a4823e92d0e15f0819c4c1d08
Kolya Zobnev <info@kolyablack.ru>
2022-09-09 07:44:02 +0300

    heading style map (#360)

--- src/ui-kit/Heading/Heading.tsx
+++ src/ui-kit/Heading/Heading.tsx
@@ -8,10 +8,10 @@ export interface HeadingProps extends HeadingContent, HeadingTypeContent {
 
 const HEADING_STYLE_MAP: Record<HeadingType, string> = {
   h1: 'text-title-xl tracking-[-0.3px]',
-  h2: 'text-title',
-  h3: 'text-title-sm',
-  h4: 'text-title-xs',
-  h5: 'text-title-2xs tracking-[0.2px]',
+  h2: 'text-title-lg',
+  h3: 'text-title',
+  h4: 'text-title-sm',
+  h5: 'text-title-xs',
   h6: 'text-title-2xs tracking-[0.2px]',
 };
 
