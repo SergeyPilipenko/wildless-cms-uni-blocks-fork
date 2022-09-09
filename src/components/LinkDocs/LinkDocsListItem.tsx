0d570c37c3c1d62aa56e3903fa1ced2722c63605
Grimitsu <Grimmreaper13@rambler.ru>
2022-09-09 11:11:15 +0300

    fix icon color on hover (#364)

--- src/components/LinkDocs/LinkDocsListItem.tsx
+++ src/components/LinkDocs/LinkDocsListItem.tsx
@@ -5,7 +5,7 @@ import { LinkDocsListItemBase } from './LinkDocsListItemBase';
 export const LinkDocsListItem = JSX<LinkDocsListItemProps>(({ className = '', doc, icon }) => (
   <LinkDocsListItemBase
     className={`flex h-full box-border ${className}`}
-    iconClassName="mr-2.5 min-w-6 min-h-6 text-primary-text"
+    iconClassName="mr-2.5 min-w-6 min-h-6"
     textClassName="self-center font-light"
     suffixClassName="text-secondary-text group-hover:text-primary-main"
     doc={doc}
