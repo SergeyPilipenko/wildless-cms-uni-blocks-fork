2fb99316e0e3ddd10838420f86e77e36829ad7c2
Longraf <32570545+Longraf@users.noreply.github.com>
2022-10-27 15:07:22 +0300

    fix input-range (#721)

--- src/ui-kit/InputRange/InputRange.tsx
+++ src/ui-kit/InputRange/InputRange.tsx
@@ -1,5 +1,6 @@
 import { JSX } from '@redneckz/uni-jsx';
 import { addSpacesBetweenNumbers } from '../../utils/addSpacesBetweenNumbers';
+import { clamp } from '../../utils/clamp';
 import type { InputRangeProps } from './InputRangeProps';
 
 const DISABLED_INPUT_CLASSES = 'disabled:border-main-stroke disabled:bg-main-divider';
@@ -16,26 +17,18 @@ export const InputRange = JSX<InputRangeProps>(
     disabled = false,
     onChange,
   }) => {
-    const handleChange = (_: string) => {
+    const getSanitizedValue = (_: string) => Number(_.replace(/\D/g, ''));
+
+    const handleChange = (e) => {
       if (!onChange) {
         return;
       }
-      const sanitizedValue = Number(_.replace(/\D/g, ''));
-      onChange(sanitizedValue);
+
+      onChange(clamp(getSanitizedValue(e.target.value as string), min, max));
     };
+
     const inputStyle = {
       backgroundSize: `${(((value - min) * 100) / (max - min)).toFixed(2)}% 100%`,
-    };
-    const handleBlur = () => {
-      if (!onChange) {
-        return;
-      }
-      if (value < min) {
-        onChange(min);
-      }
-      if (value > max) {
-        onChange(max);
-      }
     };
 
     return (
@@ -44,11 +37,11 @@ export const InputRange = JSX<InputRangeProps>(
           {title ? <span className="text-m-light">{title}</span> : null}
           <input
             className={`m-0 mt-2 font-sans text-h3 w-full h-14 border border-solid border-main-stroke rounded-md
-                        outline-none p-0 pl-4 box-border text-primary-text hover:border-primary-hover focus:border-primary-focus ${DISABLED_INPUT_CLASSES}`}
+            outline-none p-0 pl-4 box-border text-primary-text hover:border-primary-hover focus:border-primary-focus ${DISABLED_INPUT_CLASSES}`}
             value={addSpacesBetweenNumbers(value)}
             disabled={disabled}
-            onChange={(e) => handleChange(e.target.value as string)}
-            onBlur={handleBlur}
+            onChange={handleChange}
+            onBlur={handleChange}
           />
           <div className="absolute inset-x-0 -bottom-1 px-4 leading-[18px]">
             <input
@@ -59,19 +52,25 @@ export const InputRange = JSX<InputRangeProps>(
               step={step}
               value={value}
               disabled={disabled}
-              onChange={(e) => handleChange(e.target.value as string)}
+              onChange={handleChange}
               style={inputStyle}
             />
           </div>
         </label>
-        <div className="flex justify-between my-2">
-          {items.map((item, i) => (
-            <span key={String(i)} className="text-s-light text-secondary-text">
-              {item}
-            </span>
-          ))}
-        </div>
+        {renderRangeCaption(items)}
       </div>
     );
   },
 );
+
+function renderRangeCaption(items) {
+  return (
+    <div className="flex justify-between my-2">
+      {items.map((item, i) => (
+        <span key={String(i)} className="text-s-light text-secondary-text">
+          {item}
+        </span>
+      ))}
+    </div>
+  );
+}
