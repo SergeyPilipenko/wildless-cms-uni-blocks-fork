4cc8c80a27388b4640544e86cc1e84da7eccf3b2
andreySimonenko <a.dev.krd@gmail.com>
2022-11-02 16:44:46 +0300

    Fallback (#747)

--- replace-config.lib.cjs
+++ replace-config.lib.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   files: ['lib/**/*.js', 'lib-mobile/**/*.js'],
-  from: /((import|export)\s+[^'";]*\s+from\s+['"](\.\/|(\.\.\/)+)[^'";]*)['"];/g,
-  to: "$1.js';",
+  from: [
+    /((import|export)\s+[^'";]*\s+from\s+['"](\.\/|(\.\.\/)+)[^'";]*)['"];/g,
+    /(import\([`'"](\.\/|\.\.\/)+[^`'";]*)([`'";]*\))/g,
+  ],
+  to: ["$1.js';", '$1.js$3'],
   encoding: 'utf8',
 };
