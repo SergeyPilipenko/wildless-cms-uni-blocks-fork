ef20250df9038c1b4eb14f63364a76b536a59d9c
redneckz <vasyaselsky@yandex.ru>
2022-12-07 14:40:21 +0300

    Fix block examples to make it possible to declare composite blocks (blocks composed of elements)

--- src/components/ContentPage/useContentPageData.ts
+++ src/components/ContentPage/useContentPageData.ts
@@ -3,6 +3,14 @@ import type { BlocksRegistry } from '../../model/BlocksRegistry';
 import type { BlockDef, ContentPageDef } from '../../model/ContentPageDef';
 import baseData from './ContentPage.page.json';
 
+const BLOCK_TYPES_BLACK_LIST = [
+  'Header',
+  'Footer',
+  'Placeholder',
+  'AccordionItem',
+  'OtherProductsItem',
+];
+
 export function useContentPageData(blocksRegistry: BlocksRegistry): ContentPageDef {
   const [data, setData] = useState<ContentPageDef>(baseData);
 
@@ -17,12 +25,14 @@ const contentPageData = async (blocksRegistry: BlocksRegistry): Promise<ContentP
   ...baseData,
   blocks: await Promise.all(
     Object.keys(blocksRegistry)
-      .filter((type) => !['Header', 'Footer'].includes(type))
+      .filter((type) => !BLOCK_TYPES_BLACK_LIST.includes(type))
+      .sort()
       .map(
         async (type): Promise<BlockDef> => ({
           type,
-          content: await blockContentExample(type),
           style: ['col-span-12'],
+          content: {},
+          ...(await blockContentExample(type)),
         }),
       ),
   ),
